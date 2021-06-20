f948b97d93d13dd0b26cc16bdf59e9dd4663b574
Lý Phong <giaphong541@gmail.com>
2021-06-20 10:11:47 +0700

    'verson2_vietChucNangRenderProduct'

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,44 @@
 console.log('Hello Cybersoft');
+
+var arrProduct = [
+  {
+    id: 1,
+    name: "iphone X",
+    price: 1000,
+    img: "https://picsum.photos/id/1/200/200",
+  },
+  {
+    id: 2,
+    name: "iphone Xs",
+    price: 2000,
+    img: "https://picsum.photos/id/2/200/200",
+  },
+  {
+    id: 3,
+    name: "iphone Xs10",
+    price: 3000,
+    img: "https://picsum.photos/id/3/200/200",
+  },
+];
+
+function renderProduct() {
+    var content = '';
+    for (var i = 0; i < arrProduct.length; i++){
+        let product = arrProduct[i];
+        content +=
+            `<div class="col-4 mt-2">
+                <div class="card">
+                    <img src="${product.img}"/>
+                    <div class="card-body">
+                        <p>${product.price}</p>
+                        <button>Mua hàng</button>
+                    </div>
+                </div>
+            </div>
+            `
+    }
+
+    document.querySelector('#arrProduct').innerHTML = content;
+}
+
+renderProduct();
